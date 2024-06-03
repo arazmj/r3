5302fb71d55ce2250c5fd0431104346485a81be3
Amir Razmjou <arazmj@Amirs-Laptop.attlocal.net>
2024-06-02 20:44:29 -0500

    Add main controller

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-fn main() {
-    println!("Hello, world!");
-}
+mod controller;
 
+use actix_web::{App, HttpServer};
+use controller::{read_object, create_object, delete_object, update_object, version}; 
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(version)
+        .service(create_object)
+        .service(read_object)
+        .service(update_object)
+        .service(delete_object)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
