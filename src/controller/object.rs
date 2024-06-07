fe5bf8612f083e8b1904fba17be02760efbbcd0f
Amir Razmjou <arazmj@gmail.com>
2024-06-07 16:26:12 -0500

    Added read object

--- src/controller/object.rs
+++ src/controller/object.rs
@@ -1,8 +1,7 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 
-use actix_web::{get, HttpResponse, post, Responder, web};
-use bytes::Bytes;
+use actix_web::{Error, get, HttpResponse, post, Responder, web};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -11,20 +10,32 @@ struct ObjectPath {
     object: String,
 }
 
+impl ObjectPath {
+    fn into_file(&self) -> Result<File, Error> {
+        use std::path::Path;
+        let f = File::create_new(Path::new(&self.bucket).join(&self.object))?;
+        Ok(f)
+    }    
+}
+
+
 #[post("/{bucket}/{object}")]
 pub async fn create_object(path: web::Path<ObjectPath>, payload: web::Payload) 
     -> Result<impl Responder, actix_web::Error>  {
     let bytes = payload.to_bytes().await.unwrap();
-    let mut f = File::create_new(std::path::Path::new(&path.bucket).join(&path.object))?;
+    let mut f = &path.into_file()?;
     f.write_all(&bytes)?;
     println!("{}", String::from_utf8(bytes.to_vec()).unwrap());
     Ok(HttpResponse::Created().finish())
 }
 
 #[get("/{bucket}/{object}")]
-pub async fn read_object(_path: web::Path<ObjectPath>) -> impl Responder {
-    // TODO: Implement logic to read object
-    HttpResponse::Ok().body(Bytes::from_static(b"Object data"))
+pub async fn read_object(path: web::Path<ObjectPath>)
+    -> Result<impl Responder, actix_web::Error>  {
+    let mut f = &path.into_file()?;
+    let mut content =  vec![];
+    f.read_to_end(&mut content)?;
+    Ok(HttpResponse::Ok().streaming(content))
 }
 
 #[get("/{bucket}/{object}")]
